3521cc75063ec06031944efcd418c14db6bc9f28
grahamnscp <grahamnscp@gmail.com>
2025-04-22 14:18:58 +0100

    initial commit

---
+++ tf/userdata-sles.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+zypper refresh
+zypper install -y git bind-utils mlocate lvm2 jq nfs-client
+
+echo "alias l='ls -latFrh'" >> /home/ec2-user/.bashrc
+echo "alias vi=vim"         >> /home/ec2-user/.bashrc
+echo "set background=dark"  >> /home/ec2-user/.vimrc
+echo "syntax on"            >> /home/ec2-user/.vimrc
+chown ec2-user:users /home/ec2-user/.vimrc
+echo "alias l='ls -latFrh'" >> /root/.bashrc
+echo "alias vi=vim"         >> /root/.bashrc
+echo "set background=dark"  >> /root/.vimrc
+echo "syntax on"            >> /root/.vimrc
+
+# Add PackageHub Repo
+SUSEConnect -p PackageHub/15.6/x86_64
+zypper refresh
+
+updatedb
+
